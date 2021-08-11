5bd79069a8317a13e55080d7646cc8ec8da8d51f
vflorelo <vflorelo@gmail.com>
2021-08-11 22:31:21 +0100

    working version

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$output = shell_exec("python search_elsevier.py \"Plasmodium falciparum AND 26S proteasome regulatory subunit RPN1\" > test.xml 2> test.err") ;
-$output = shell_exec("cat test.xml test.err");
-print $output;
-?>
